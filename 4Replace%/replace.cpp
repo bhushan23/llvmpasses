689887895d2e6989cc1337dab880bbc9b95366d2
Bhushan Sonawane <bhushan.s.94@gmail.com>
2014-08-04 20:04:36 +0530

    apint

--- 4Replace%/replace.cpp
+++ 4Replace%/replace.cpp
@@ -1,3 +1,4 @@
+#include "llvm/ADT/APFloat.h"
 #include "llvm/Pass.h"
 #include "llvm/PassAnalysisSupport.h"
 #include "llvm/IR/Function.h"
@@ -9,7 +10,10 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Support/InstIterator.h"
 #include "llvm/IR/User.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/ADT/APInt.h"
 #include "vector"
+ 
 using namespace llvm;
 
 
@@ -29,12 +33,24 @@ namespace {
 										if( I->getOpcode() == 17 ){//18: opcode enum for srem
 												errs() << I->getOpcode() << "   " << I->getOpcodeName() << "  " << *(I->getOperand(0)) << " " << *(I->getOperand(1))<<"\n";
 												int op1;
-												Value *v1 = I->getOperand(0);
-												llvm::ConstantInt* CI = dyn_cast<llvm::ConstantInt> (v1);
-												errs() << "got 1 operand " << *CI;
-											//	if( ( dyn_cast<ConstantInt>(*(I->getOperand(1))) & dyn_cast<ConstantInt>(*(I->getOperand(1)-1)) ) == 0 ){
-											//		errs() << "Oeprand is multiple\n";
-											//	}
+												Value *v1 = (I->getOperand(1));
+												//errs() << *v1 <<" " << *v1-1;
+												//llvm::ConstantInt *CI = cast<llvm::Constant_int>(*(I->getOperand(1)));
+										//		errs() << "\n::: " << *(v1->getType());
+												//llvm::ConstantInt* CI = dyn_cast<llvm::ConstantInt> (v1);
+												//errs() << "got 1 operand " << *CI;
+										/*		if( ( (*(I->getOperand(1))) & (*(I->getOperand(1))) ) == 0 ){
+													errs() << "Oeprand is multiple\n";
+												}*/
+											
+												if (ConstantInt* CI = dyn_cast<ConstantInt>(v1)) {
+													errs() << "\nconst found";
+													APInt *ap = dyn_cast <APInt>(v1);
+															
+												}
+												else {
+													errs() << "\nNot const found";
+												}
 										}
 								}
 
